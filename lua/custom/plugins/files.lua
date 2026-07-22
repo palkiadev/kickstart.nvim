9de26dfbaf2e112f8672d68f170012fec80ecb24
palkiaex <kimhuyss@icloud.com>
2026-07-22 18:54:03 +0700

    oil: Hide .git, .DS_Store and ..

--- lua/custom/plugins/files.lua
+++ lua/custom/plugins/files.lua
@@ -4,6 +4,9 @@ require('oil').setup({
   skip_confirm_for_simple_edits = true,
   view_options = {
     show_hidden = true,
+    is_always_hidden = function(name, bufnr)
+      return name == ".git" or name == ".DS_Store" or name == ".."
+    end,
   },
   keymaps = {
     ["<leader>to"] = {
