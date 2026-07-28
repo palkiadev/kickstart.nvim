a5aa8e5ece025bef0835e9c9ddf4ac17511ca1d9
palkiadev <kimhuy9119@gmail.com>
2026-07-28 15:57:52 +0700

    Use lazydev for lua

--- init.lua
+++ init.lua
@@ -625,41 +625,10 @@ do
         },
       },
     },
-    --
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
-    --
-    -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
 
-    stylua = {}, -- Used to format Lua code
+    stylua = {},
 
-    -- Special Lua Config, as recommended by neovim help docs
     lua_ls = {
-      on_init = function(client)
-        client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
-
-        if client.workspace_folders then
-          local path = client.workspace_folders[1].name
-          if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-        end
-
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-          runtime = {
-            version = 'LuaJIT',
-            path = { 'lua/?.lua', 'lua/?/init.lua' },
-          },
-          workspace = {
-            checkThirdParty = false,
-            -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-            --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-            library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-              '${3rd}/luv/library',
-              '${3rd}/busted/library',
-            }),
-          },
-        })
-      end,
       ---@type lspconfig.settings.lua_ls
       settings = {
         Lua = {
@@ -674,7 +643,10 @@ do
     gh 'mason-org/mason.nvim',
     gh 'mason-org/mason-lspconfig.nvim',
     gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
+    gh 'folke/lazydev.nvim'
   }
+
+  require('lazydev').setup {}
 
   -- Automatically install LSPs and related tools to stdpath for Neovim
   require('mason').setup {}
