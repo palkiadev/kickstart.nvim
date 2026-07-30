d1a4f81052ba9d88e6767e56e5e25be31b41337b
palkiaex <kimhuyss@icloud.com>
2026-07-30 08:04:45 +0700

    Update gruvbox background

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -12,7 +12,7 @@ vim.pack.add(themes)
 
 vim.g.everforest_background = 'hard'
 vim.g.everforest_enable_italic = true
-vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'material'
 vim.g.gruvbox_material_enable_italic = true
 
